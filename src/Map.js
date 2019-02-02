c6df346b7eded68ab2e88b8c0f9d0f3b6db52ab4
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-02 17:35:35 +0000

    Added api key as apiKey state in <Map/>

--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Map extends Component {
+  state = {
+    apiKey: process.env.REACT_APP_MAP_API_KEY
+  }
+
   render() {
     return (
       <div className="Map">

e52194e70934faa3c67d7c8e88b78d4469aed2f2
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-02 16:08:39 +0000

    Created <Map/> Component

---
+++ src/Map.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="Map">
+      </div>
+    );
+  }
+}
+
+export default Map;

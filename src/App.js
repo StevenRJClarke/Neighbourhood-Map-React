d673a6710757f7f5161ececc7b6302ac9e4c630e
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-04 19:18:48 +0000

    Added locations state up to <App/> from <Map/>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Map from './Map'
 import './App.css';
 
 class App extends Component {
+  state = {
+    locations: [
+      { title: 'Richmond Park' },
+      { title: 'Hampton Court' },
+      { title: 'Wimbledon Common' },
+      { title: 'Chessington World of Adventures' },
+      { title: 'Clapham Common' }
+    ]
+  }
+
   render() {
     return (
       <div className="App">

e1cd819f48628fce10b7e3b14d62b66f31042b93
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-06 18:33:30 +0000

    Created <LocationFilter/> Component

---
+++ src/LocationFilter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class LocationFilter extends Component {
+  render() {
+    return (
+      <div className="location-filter">
+      </div>
+    );
+  }
+}
+
+export default LocationFilter;

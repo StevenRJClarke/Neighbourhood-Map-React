76f0a3db5836fea232e3e2f4bf61ef3205208dec
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-07 06:28:19 +0000

    Added query state in <LocationFilter/>

--- src/LocationFilter.js
+++ src/LocationFilter.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class LocationFilter extends Component {
+  state = {
+    query: ''
+  }
+
   render() {
     return (
       <div className="location-filter">

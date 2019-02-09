9f8859bae08213c92c95c2435096e1cd5f0d3ed5
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-09 13:34:50 +0000

    <Sidebar/> passes getFilter() fn to <LocationFilter/> to lift up state

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,7 +6,9 @@ class Sidebar extends Component {
   render() {
     return (
       <div className="Sidebar">
-        <LocationFilter/>
+        <LocationFilter
+          getFilter={this.props.getFilter}
+        />
         <ul className="locations-list">
           {this.props.locations.map( location => (
             <li key={location.placeId} className="location-item">{location.name}</li>

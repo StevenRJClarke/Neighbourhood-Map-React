984c2b6a0f1324aeccaecbd045170901acfabd1e
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-10 11:32:06 +0000

    sortBy used to put locations in alphabetical order

--- src/App.js
+++ src/App.js
@@ -41,6 +41,8 @@ class App extends Component {
       showingLocations = this.state.locations;
     }
 
+    showingLocations.sort(sortBy('name'));
+
     return (
       <div className="App">
         <Sidebar

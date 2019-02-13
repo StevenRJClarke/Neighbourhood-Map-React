d3b340615623cc4d320046bb073a681504010606
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-13 07:38:40 +0000

    Fetched venue information of locations and returned Response object

--- src/Map.js
+++ src/Map.js
@@ -268,8 +268,15 @@ class Map extends Component {
   // Get more detailed information about the location to show on the infowindow
   // @param {Object} marker - google.maps.Marker
   getFoursquareInfo(marker) {
-    fetch()
-    .then()
+    fetch(`https://api.foursquare.com/v2/venues/explore?client_id=GUA2IQTSLKUOJ1YDJNHF5VJ2FWIAQQNLXJH2ISPWQ2BTCFOG&client_secret=TCLGU4JSWBSNLMMF42UU5YJXPJKHRTHXJJDNVDJ1RK25GX0V&v=20180323&limit=1&ll=${marker.position.lat()},${marker.position.lng()}`)
+    .then(
+      // Get response object from returned Promise
+      response => response.json()
+    )
+    .then(
+      // Get response object
+      response => {}
+    )
     .catch();
   }
 

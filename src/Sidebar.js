02b3c79997bd2e9f8641f2d689ca72963222ca35
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-14 21:21:46 +0000

    Added onKeyPress to sidebar list so pressing Enter same as click

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -13,6 +13,7 @@ class Sidebar extends Component {
           {this.props.locations.map( location => (
             <li key={location.placeId} className="location-item" tabIndex="0"
               onClick={ event => this.props.handleClickOnSidebar(event.target.innerHTML) }
+              onKeyPress={ event => (event.key === "Enter") && this.props.handleClickOnSidebar(event.target.innerHTML) }
             >{location.name}</li>
           ))}
         </ul>

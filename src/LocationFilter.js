5e16f134f6bb3e1c14c404bea70e94278d22b90f
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-15 07:58:29 +0000

    Removed placeholder from <input> now there is a <label>

--- src/LocationFilter.js
+++ src/LocationFilter.js
@@ -14,8 +14,8 @@ class LocationFilter extends Component {
   render() {
     return (
       <div className="location-filter">
-        <label for="location-input">Type in a location to filter the locations on the map</label>
-        <input id="location-input" type="text" placeholder="Filter locations"
+        <label>Type in a location to filter the locations on the map</label>
+        <input id="location-input" type="text"
           value={this.state.query}
           onChange={ event => {
             this.updateQuery(event.target.value);

73215623ab48b031f9ace962608358a5fe5d4187
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-15 22:18:02 +0000

    Last location in list uses different aria-describedby

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,13 +12,25 @@ class Sidebar extends Component {
         <div id="locations-item-description" hidden>
           This is one of the locations displayed on the map. Press enter to find out more about this location. Press tab to hear the next location.
         </div>
-        <ul className="locations-list" role="menu">
-          {this.props.locations.map( location => (
-            <li key={location.placeId} className="location-item" tabIndex="0" role="menuitem" aria-describedby="locations-item-description"
-              onClick={ event => this.props.handleClickOnSidebar(event.target.innerHTML) }
-              onKeyPress={ event => (event.key === "Enter") && this.props.handleClickOnSidebar(event.target.innerHTML) }
-            >{location.name}</li>
-          ))}
+        <div id="last-locations-item-description" hidden>
+          This is one of the locations displayed on the map. Press enter to find out more about this location.
+        </div>
+        <ul className="locations-list">
+          {this.props.locations.map( (location, index) =>
+            (
+              index < this.props.locations.length - 1
+            ) ? (
+              <li key={location.placeId} className="location-item" tabIndex="0" aria-describedby="locations-item-description"
+                onClick={ event => this.props.handleClickOnSidebar(event.target.innerHTML) }
+                onKeyPress={ event => (event.key === "Enter") && this.props.handleClickOnSidebar(event.target.innerHTML) }
+              >{location.name}</li>
+            ) : (
+              <li key={location.placeId} className="location-item" tabIndex="0" aria-describedby="last-locations-item-description"
+                onClick={ event => this.props.handleClickOnSidebar(event.target.innerHTML) }
+                onKeyPress={ event => (event.key === "Enter") && this.props.handleClickOnSidebar(event.target.innerHTML) }
+              >{location.name}</li>
+            )
+          )}
         </ul>
       </div>
     );
